76f3c2a3818aad806cf87b9d428c898c762263d8
Tony K Tan <tankwantiong@gmail.com>
2016-06-26 11:31:47 +0800

    add frontend views of auntie-kindness app

---
+++ auntie-signup-view/js/auntieview.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  $(".buttonfirst").click(function(){
+    $(".dialoguefirst").hide();
+    $(".imgfirst").hide();
+    $(".formfirst").hide();
+    $(".buttonfirst").hide();
+    $(".dialoguesecond").fadeIn();
+    $(".imgsecond").fadeIn();
+  });
+
+});
